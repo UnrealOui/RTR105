d3f14fc351bec52caa16c5d6b3f4c7e3239e14e2
user <akom211199@gmail.com>
2018-10-02 15:50:23 +0300

    20181002_15_50

---
+++ 1st_prog.py
@@ -0,0 +1,24 @@
+"""
+hrs = input("Enter Hours:")
+h = float(hrs)
+sh = 40
+rat = 10.50
+rcf = 1.5
+if h > 40:
+    a = h - sh
+    r = rat * rcf
+    x = a * r + sh*rat
+    print(x)
+else:
+    x = h * rat
+    print(x)
+"""    
+astr = "Bob"
+try:
+    print("Hello")
+    istr = str(astr)
+    print("There")
+except:
+    istr = -1
+    
+print("Done", istr)
