b88652128225046fde7231b5b38422ed6c3a4b49
user <akom211199@gmail.com>
2018-10-16 14:28:33 +0300

    20181016_14_10

---
+++ test_5_20181016.py
@@ -0,0 +1,38 @@
+"""
+n = 5
+while n > 0:
+    print(n)
+    n = n - 1
+print("Blastoff!")
+print(n)
+"""
+"""
+#infinity
+n = 5
+while n > 0:
+    print("Blastoff!")
+print(n)
+"""
+"""
+while True:
+    line = raw_input(">")
+    if line == "done":
+        break
+    print(line)
+print("Done!")
+"""
+"""
+while True:
+    line = raw_input(">")
+    if line[0] == "#":
+        continue
+    if line == "done":
+        break
+    print(line)
+print("Done!")
+"""
+"""
+for i in [5,4,3,2,1]:
+    print(i)
+print("Blastoff!")
+"""
